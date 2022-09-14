15cc5a4049f209050f586982361f02c61da80f2b
Romataka <atakaesson24@gmail.com>
2022-09-14 15:42:21 +0100

    Worked on the header to make dropdown menu and make it a bit responsive .

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+const button = document.querySelector('.dropbtn')
+
+button.addEventListener('click', function(){
+    // alert('Hello')
+    const list = document.querySelector('.dropdown-content1')
+    if(list.style.display == 'none'){
+        list.style.display = 'block'
+    } else {list.style.display = 'none' }
+
+})
+
+
+const button2 = document.querySelector('.dropbtn2')
+
+button2.addEventListener('click', function(){
+    // alert('Hello')
+    const list = document.querySelector('.dropdown-content2')
+    if(list.style.display == 'none'){
+        list.style.display = 'block'
+    } else {list.style.display = 'none' }
+
+})
