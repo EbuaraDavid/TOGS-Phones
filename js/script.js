93d8bc282edc4f01057d1285127b09cce99406b6
Abraham Bage <104463663+codith@users.noreply.github.com>
2022-09-14 08:47:39 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.dropbtn')
+
+button.addEventListener('click', function(){
+    // alert('Hello')
+    const list = document.querySelector('.dropdown-content1')
+    if(list.style.display == 'none'){
+        list.style.display = 'block'
+    } else {list.style.display = 'none' }
+
+})
+
