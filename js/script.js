86373b3d885052b0513fb6861104c67575195343
Ebuara David <ebuaradavidd@gmail.com>
2022-09-14 17:42:58 -0700

    neww

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const button = document.querySelector(".dropbtn");
 
 button.addEventListener("click", function () {
@@ -21,3 +22,16 @@ button2.addEventListener("click", function () {
     list.style.display = "none";
   }
 });
+=======
+const button = document.querySelector('.dropbtn')
+
+button.addEventListener('click', function(){
+    // alert('Hello')
+    const list = document.querySelector('.dropdown-content1')
+    if(list.style.display == 'none'){
+        list.style.display = 'block'
+    } else {list.style.display = 'none' }
+
+})
+
+>>>>>>> fb384782cd877aa53c29bb3bef18bb1ae27b5a85
